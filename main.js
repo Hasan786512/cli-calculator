198555b9a65b4c7cd509c852074f11f225e74b8b
Hasan Raza <hasanraza230@gmail.com>
2024-03-28 07:12:13 +0500

    This is my Simple calculator to push in github

---
+++ main.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answear = await inquirer.prompt([{
+        name: "firstnumber",
+        type: "number",
+        message: "enter first number",
+    },
+    { name: "secondnumber",
+        type: "number",
+        message: "enter second number", },
+    { name: "operator",
+        type: "list",
+        message: "enter a number to perform operator",
+        choices: ["add", "multi", "sub", "div"],
+    }
+]);
+if (answear.operator === "add") {
+    console.log(answear.firstnumber + answear.secondnumber);
+}
+else if (answear.operator === "multi") {
+    console.log(answear.firstnumber * answear.secondnumber);
+}
+else if (answear.operator === "sub") {
+    console.log(answear.firstnumber - answear.secondnumber);
+}
+else if (answear.operator === "div") {
+    console.log(answear.firstnumber / answear.secondnumber);
+}
+else
+    console.log("sorry you chooose wrong operator");
